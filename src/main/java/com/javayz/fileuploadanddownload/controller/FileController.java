5d2da802b5bc787e9345285e083054b42b0625b7
OliverLiy <974474148@qq.com>
2021-04-15 23:11:37 +0800

    update

--- src/main/java/com/javayz/fileuploadanddownload/controller/FileController.java
+++ src/main/java/com/javayz/fileuploadanddownload/controller/FileController.java
@@ -13,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -43,13 +44,22 @@ public class FileController {
     public void downloadFiles(@PathVariable("id") String id, HttpServletRequest request, HttpServletResponse response){
         OutputStream outputStream=null;
         InputStream inputStream=null;
+        BufferedInputStream bufferedInputStream=null;
+        byte[] bytes=new byte[1024];
         Files files = fileService.getFileById(id);
         String fileName = files.getFileName();
-        inputStream=fileService.getFileInputStream(files);
-        response.setHeader("Content-Disposition", "attachment;filename=" +  new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1));
         // 获取输出流
         try {
+            response.setHeader("Content-Disposition", "attachment;filename=" +  new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1));
+            response.setContentType("application/force-download");
+            inputStream=fileService.getFileInputStream(files);
+            bufferedInputStream=new BufferedInputStream(inputStream);
             outputStream = response.getOutputStream();
+            int i=bufferedInputStream.read(bytes);
+            while (i!=-1){
+                outputStream.write(bytes,0,i);
+                i=bufferedInputStream.read(bytes);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }finally {
@@ -59,6 +69,9 @@ public class FileController {
                 }
                 if (outputStream!=null){
                     outputStream.close();
+                }
+                if (bufferedInputStream!=null){
+                    bufferedInputStream.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
